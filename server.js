2ca696aa2298cf1761d10dda316fd7a78ae06380
Vsumargo <vincent.sumargo@gmail.com>
2021-03-30 23:59:17 +0800

    initial commit; added initial service-worker & manifest

---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/html.js"));
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
